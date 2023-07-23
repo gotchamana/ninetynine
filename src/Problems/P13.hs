abaef974452d8eb1099665ee231104541d334277
shootingstar <jinhung.c@gmail.com>
2023-07-23 22:48:04 +0800

    feat: Problem 13

--- src/Problems/P13.hs
+++ src/Problems/P13.hs
@@ -8,8 +8,7 @@ Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.P13".
 -}
 module Problems.P13 (encodeDirect) where
 
-import           Problems.Lists
-import qualified Solutions.P13  as Solution
+import Problems.Lists (Encoding (..))
 
 -- | Implement the so-called run-length encoding data compression method directly.
 -- I.e., do not explicitly create the sublists containing the duplicates,
@@ -23,4 +22,15 @@ import qualified Solutions.P13  as Solution
 -- >>> encodeDirect "aaaabccaadeeee"
 -- [Multiple 4 'a',Single 'b',Multiple 2 'c',Multiple 2 'a',Single 'd',Multiple 4 'e']
 encodeDirect :: Eq a => [a] -> [Encoding a]
-encodeDirect = Solution.encodeDirect
+encodeDirect = foldr f []
+  where
+    f e [] = [Single e]
+    f e acc@(x : xs) = if e == getElement x then increment x : xs else Single e : acc
+
+getElement :: Encoding a -> a
+getElement (Single e) = e
+getElement (Multiple _ e) = e
+
+increment :: Encoding a -> Encoding a
+increment (Single e) = Multiple 2 e
+increment (Multiple n e) = let !times = n + 1 in Multiple times e

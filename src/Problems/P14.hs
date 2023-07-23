4063cbe0b033e60bf472f1600a90bd37e6c77813
shootingstar <jinhung.c@gmail.com>
2023-07-23 22:54:09 +0800

    feat: Problem 14

--- src/Problems/P14.hs
+++ src/Problems/P14.hs
@@ -8,8 +8,6 @@ Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.P14".
 -}
 module Problems.P14 (dupli) where
 
-import qualified Solutions.P14 as Solution
-
 -- | Duplicate the elements of a list.
 --
 -- === Examples
@@ -17,4 +15,4 @@ import qualified Solutions.P14 as Solution
 -- >>> dupli [1, 2, 3]
 -- [1,1,2,2,3,3]
 dupli :: [a] -> [a]
-dupli = Solution.dupli
+dupli = concatMap (\e -> [e, e])

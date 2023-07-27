f99289461c7e10dd17fdc31939f5cc7f3e04333c
shootingstar <jinhung.c@gmail.com>
2023-07-27 09:52:58 +0800

    feat: Problem 20

--- src/Problems/P20.hs
+++ src/Problems/P20.hs
@@ -8,8 +8,6 @@ Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.P20".
 -}
 module Problems.P20 (removeAt) where
 
-import qualified Solutions.P20 as Solution
-
 -- | Remove the @k@th element from a list.
 -- Return the element removed and the residue list.
 --
@@ -17,5 +15,11 @@ import qualified Solutions.P20 as Solution
 --
 -- >>> removeAt 2 "abcd"
 -- ('b',"acd")
-removeAt :: Int -> [a] -> (a,[a])
-removeAt = Solution.removeAt
+removeAt :: Int -> [a] -> (a, [a])
+removeAt _ [] = error "Empty list"
+removeAt 1 (x : xs) = (x, xs)
+removeAt n (x : xs) =
+    let (y, ys) = removeAt (n - 1) xs
+     in if n <= 0
+            then error $ "Invalid index: " <> show n
+            else (y, x : ys)

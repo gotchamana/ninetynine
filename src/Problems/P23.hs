e8c1a1ab6b6789131078f23254a28286126864e8
shootingstar <jinhung.c@gmail.com>
2023-07-29 23:56:43 +0800

    feat: Problem 23

--- src/Problems/P23.hs
+++ src/Problems/P23.hs
@@ -8,8 +8,13 @@ Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.P23".
 -}
 module Problems.P23 (randomSelect) where
 
-import qualified Solutions.P23 as Solution
-import           System.Random
+import Control.Monad (forM_)
+import Control.Monad.ST (ST, runST)
+import Control.Monad.State.Strict (MonadState (get, put), MonadTrans (lift), execStateT)
+import Data.Array.MArray (MArray (getBounds), newListArray, readArray, writeArray, getElems)
+import Data.Array.ST (STArray)
+import Data.Ix (Ix)
+import System.Random (Random (randomR), RandomGen)
 
 -- | Extract a given number of randomly selected elements from a list.
 --
@@ -26,5 +31,34 @@ import           System.Random
 --
 -- >>> fst . randomSelect "abcdefgh" 3 <$> newStdGen
 -- "ebf"
-randomSelect :: RandomGen g => [a] -> Int -> g -> ([a], g)
-randomSelect = Solution.randomSelect
+randomSelect :: forall a g. RandomGen g => [a] -> Int -> g -> ([a], g)
+randomSelect [] _ g = ([], g)
+randomSelect xs n g =
+    if n <= 0
+        then ([], g)
+        else runST $ do
+            arr <- newListArray (1, length xs) xs :: ST s (STArray s Int a)
+            g' <- shuffle arr g
+            ys <- take n <$> getElems arr
+
+            return (ys, g')
+
+shuffle :: (RandomGen g, MArray a e m) => a Int e -> g -> m g
+shuffle arr = execStateT $ do
+    (low, high) <- lift $ getBounds arr
+
+    forM_ [low .. high - 1] $ \i -> do
+        g <- get
+
+        let (j, g') = randomR (i, high) g
+        put g'
+
+        lift $ swap arr i j
+
+swap :: (MArray a e m, Ix i) => a i e -> i -> i -> m ()
+swap arr i j = do
+    e1 <- readArray arr i
+    e2 <- readArray arr j
+
+    writeArray arr i e2
+    writeArray arr j e1

3fcaa5059c5861a4686866ce7fc731c9a6a3bf92
shootingstar <jinhung.c@gmail.com>
2023-08-13 00:32:58 +0800

    feat: Problem 33

--- src/Problems/P33.hs
+++ src/Problems/P33.hs
@@ -8,8 +8,6 @@ Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.P33".
 -}
 module Problems.P33 (coprime) where
 
-import qualified Solutions.P33 as Solution
-
 -- | Determine whether two positive integer numbers are coprime.
 -- Two numbers are coprime if their greatest common divisor equals 1.
 --
@@ -21,4 +19,4 @@ import qualified Solutions.P33 as Solution
 -- >>> coprime 1173 1547
 -- False
 coprime :: Integral a => a -> a -> Bool
-coprime = Solution.coprime
+coprime a b = gcd a b == 1

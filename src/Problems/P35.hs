7c3e27e9761bf723a952f3115934ef4890f9b61a
shootingstar <jinhung.c@gmail.com>
2023-08-13 21:03:24 +0800

    feat: Problem 35

--- src/Problems/P35.hs
+++ src/Problems/P35.hs
@@ -8,7 +8,12 @@ Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.P35".
 -}
 module Problems.P35 (primeFactors) where
 
-import qualified Solutions.P35 as Solution
+import Control.Monad (guard)
+import Control.Monad.State.Strict (MonadState (get, put), execState)
+import Control.Monad.Trans.Maybe (MaybeT (runMaybeT))
+import Data.Foldable (Foldable (toList), foldlM)
+import Data.Sequence qualified as Seq
+import Data.Sequence ((|>))
 
 -- | Determine the prime factors of a given positive integer.
 -- Construct a list containing the prime factors in ascending order.
@@ -17,5 +22,30 @@ import qualified Solutions.P35 as Solution
 --
 -- >>> primeFactors 315
 -- [3,3,5,7]
-primeFactors :: Integral a => a -> [a]
-primeFactors = Solution.primeFactors
+primeFactors :: forall a. Integral a => a -> [a]
+primeFactors n =
+    if n <= 1
+        then []
+        else
+            let s = (n, Seq.empty)
+                (n', factors) = execState (runMaybeT computation) s
+             in toList $ if n' /= 1 then factors |> n' else factors
+  where
+    sqrtN = truncate @Double @a . sqrt . fromIntegral $ n
+    computation = foldlM f () [2 .. sqrtN]
+    f _ fac = do
+        (n', result) <- get
+
+        guard (n' /= 1)
+
+        let (q, p) = getPower n' fac
+        put (q, result <> Seq.replicate (fromIntegral p) fac)
+
+getPower :: Integral a => a -> a -> (a, a)
+getPower n 0 = (n, 0)
+getPower 0 _ = (0, 0)
+getPower n f = go n 0
+  where
+    go n' !acc =
+        let (q, r) = n' `divMod` f
+         in if r == 0 then go q (acc + 1) else (n', acc)

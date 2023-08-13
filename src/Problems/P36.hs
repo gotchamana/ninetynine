23e1775bc7556e8c1289d0cd7da48a78362fbf86
shootingstar <jinhung.c@gmail.com>
2023-08-13 21:37:36 +0800

    feat: Problem 36

--- src/Problems/P36.hs
+++ src/Problems/P36.hs
@@ -8,7 +8,8 @@ Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.P36".
 -}
 module Problems.P36 (primeFactorsMultiplicity) where
 
-import qualified Solutions.P36 as Solution
+import Data.List.NonEmpty (group, NonEmpty ((:|)))
+import Problems.P35 (primeFactors)
 
 -- | Determine the prime factors of a given positive integer.
 -- Construct a list containing the prime factors and their multiplicity.
@@ -18,4 +19,4 @@ import qualified Solutions.P36 as Solution
 -- >>> primeFactorsMultiplicity 315
 -- [(3,2),(5,1),(7,1)]
 primeFactorsMultiplicity :: Integral a => a -> [(a, a)]
-primeFactorsMultiplicity = Solution.primeFactorsMultiplicity
+primeFactorsMultiplicity = map (\xs@(x :| _) -> (x, fromIntegral $ length xs)) . group . primeFactors
